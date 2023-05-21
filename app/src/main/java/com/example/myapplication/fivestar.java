41e6a8bde4c6eca8be25121a3b5826060add57a7
SUYOUNG-YOO <suyoung_99@naver.com>
2023-05-21 17:42:29 +0900

    홈버튼 수정및기능

--- app/src/main/java/com/example/myapplication/fivestar.java
+++ app/src/main/java/com/example/myapplication/fivestar.java
@@ -2,16 +2,29 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class fivestar extends AppCompatActivity {
 
+    private Button homebtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.star);
+
+        homebtn = findViewById(R.id.homebtn);
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(fivestar.this, File.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
